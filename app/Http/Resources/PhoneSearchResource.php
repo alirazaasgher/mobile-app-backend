b4e3ad4c8939e72cb402cd000bb63d9979552ad4
alirazaasgher <alirazaasgher21@gmail.com>
2025-12-02 00:50:50 +0500

    C:/Program Files/Git/

--- app/Http/Resources/PhoneSearchResource.php
+++ app/Http/Resources/PhoneSearchResource.php
@@ -14,6 +14,7 @@ class PhoneSearchResource extends JsonResource
             'ram' => !empty($ramOptions) ? min($ramOptions) : null,
             'storage' => !empty($storageOptions) ? min($storageOptions) : null,
             'min_price' => number_format($this->min_price_pkr, 0, '.', ','),
+            json_decode($this->specs_grid, true),
         ];
 
         // Only include top_specs and specs_grid if request has details page flag
