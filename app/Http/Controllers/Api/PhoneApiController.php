60c647a37167e4777286b6f0318a3410042f6874
alirazaasgher <alirazaasgher21@gmail.com>
2025-12-09 01:33:41 +0500

    new changes

--- app/Http/Controllers/Api/PhoneApiController.php
+++ app/Http/Controllers/Api/PhoneApiController.php
@@ -157,7 +157,7 @@ class PhoneApiController extends Controller
             ->limit(6)
             ->get(['id', 'name', 'slug', 'primary_image', 'brand_id']);
 
-        //dd($similarMobiles->toSql());
+        dd($similarMobiles->toSql(), $similarMobiles->bindings);
         // ->get(['id', 'name', 'slug', 'primary_image', 'brand_id']);
         return response()->json([
             'success' => true,
