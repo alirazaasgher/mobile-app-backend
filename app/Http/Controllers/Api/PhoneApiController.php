545b7a6c39b6c78096948c77b4e629cfd4fe50a4
alirazaasgher <alirazaasgher21@gmail.com>
2025-12-09 01:31:38 +0500

    new changes

--- app/Http/Controllers/Api/PhoneApiController.php
+++ app/Http/Controllers/Api/PhoneApiController.php
@@ -154,10 +154,10 @@ class PhoneApiController extends Controller
                             ->orWhereBetween('max_price_pkr', $priceRange);
                     });
             })
-            ->limit(6);
-        // ->get(['id', 'name', 'slug', 'primary_image', 'brand_id']);
+            ->limit(6)
+            ->get(['id', 'name', 'slug', 'primary_image', 'brand_id']);
 
-        dd($similarMobiles->toSql());
+        //dd($similarMobiles->toSql());
         // ->get(['id', 'name', 'slug', 'primary_image', 'brand_id']);
         return response()->json([
             'success' => true,
