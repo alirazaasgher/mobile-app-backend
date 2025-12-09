91f93c9d418be909edc16c0a035988faec622e32
alirazaasgher <alirazaasgher21@gmail.com>
2025-12-10 01:23:08 +0500

    new changes

--- app/Http/Resources/PhoneSearchResource.php
+++ app/Http/Resources/PhoneSearchResource.php
@@ -39,9 +39,11 @@ class PhoneSearchResource extends JsonResource
             'min_price' => isset($this->min_price_pkr) && $this->min_price_pkr > 0
                 ? number_format($this->min_price_pkr, 0, '.', ',')
                 : null,
-            'specs_grid' => json_decode($this->specs_grid, true)
+            'specs_grid' => json_decode($this->specs_grid, true),
+            'min_price_usd' => $this->min_price_usd,
+            true
         ];
-        // !$this->hideDetails && 
+        // !$this->hideDetails &&
         // Only include top_specs and specs_grid if request has details page flag
         if (!$this->hideDetails && ($request->query('details') || $request->routeIs('phones.show'))) {
             $data['top_specs'] = json_decode($this->top_specs, true);
