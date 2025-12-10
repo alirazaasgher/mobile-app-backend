c3cb579f7f0c055d8ac6db525ed84df5c20c5d63
alirazaasgher <alirazaasgher21@gmail.com>
2025-12-11 01:09:38 +0500

    new changes

--- app/Http/Resources/PhoneImageResource.php
+++ app/Http/Resources/PhoneImageResource.php
@@ -5,16 +5,16 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class PhoneImageResource extends JsonResource
 {
-  public function toArray($request)
-{
-    $baseUrl = config('app.url'); // or env('APP_URL')
-    
-    return [
-        'id' => $this->id,
-        'url' => $this->image_url 
-            ? $baseUrl . '/storage/' . ltrim($this->image_url, '/')
-            : null,
-        'sort' => $this->sort,
-    ];
-}
+    public function toArray($request)
+    {
+        // $baseUrl = config('app.url'); // or env('APP_URL')
+        $baseUrl = "https://cdn.mobile42.com";
+        return [
+            'id' => $this->id,
+            'url' => $this->image_url
+                ? $baseUrl . '/storage/' . ltrim($this->image_url, '/')
+                : null,
+            'sort' => $this->sort,
+        ];
+    }
 }
