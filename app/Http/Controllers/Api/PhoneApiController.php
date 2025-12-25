f6824f4f9d542ac1aeb6255f86059486fa7ec179
alirazaasgher <alirazaasgher21@gmail.com>
2025-12-26 02:17:43 +0500

    new changes

--- app/Http/Controllers/Api/PhoneApiController.php
+++ app/Http/Controllers/Api/PhoneApiController.php
@@ -31,8 +31,8 @@ class PhoneApiController extends Controller
     {
         $cacheTTL = 43200;
         $usedPhoneIds = [];
-        $baseQuery = Phone::select('id','brand:id,name', 'name', 'slug', 'release_date', 'primary_image', 'status', 'updated_at')
-            ->with(['searchIndex'])
+        $baseQuery = Phone::select('id','name', 'slug', 'release_date', 'primary_image', 'status', 'updated_at')
+            ->with(['searchIndex','brand:id,name'])
             ->active();
 
         // Cache latest mobiles
