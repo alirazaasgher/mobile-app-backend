a515782e01b7e4378490d9b0f87b4b5c093d037a
alirazaasgher <alirazaasgher21@gmail.com>
2025-12-26 21:56:09 +0500

    new changes

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -101,7 +101,7 @@ function update_phone_search_index(
             'screen_size_inches' => $sizeInInches,
             'battery_capacity_mah' => $capacity,
             'main_camera_mp' => $setup,
-            'selfie_camera_mp' => $selfieCammp ?? '',
+            'selfie_camera_mp' => $selfieCammp ?? 0,
             'os' => $os,
             'chipset' => $chipset,
             'has_5g' => $has5G,
