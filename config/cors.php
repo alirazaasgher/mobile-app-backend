4a320ed3e223979cb30ab1a3ac4e47b78d8709be
alirazaasgher <alirazaasgher21@gmail.com>
2025-12-31 02:05:16 +0500

    new changes

--- config/cors.php
+++ config/cors.php
@@ -1,15 +1,29 @@
-
 <?php
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'http://localhost:3000', // Next.js development
-        'https://www.mobile42.com'
+        'http://localhost:3000',
+        'https://www.mobile42.com',
+        'https://mobile42.com',
     ],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [
+        'Content-Type',
+        'Authorization',
+        'X-Requested-With',
+        'X-XSRF-Token',
+        'Accept',
+        'X-CLIENT-ID',
+        'X-TIMESTAMP',
+        'X-NONCE',
+        'X-SIGNATURE',
+        'x-client-id',
+        'x-timestamp',
+        'x-nonce',
+        'x-signature',
+    ],
     'exposed_headers' => ['X-API-Version', 'X-RateLimit-Remaining'],
-    'max_age' => 0,
+    'max_age' => 3600,
     'supports_credentials' => false,
 ];
