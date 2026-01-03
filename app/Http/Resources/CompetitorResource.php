353d4130a1febb2223e1d7a93ee04923bb6c5bda
alirazaasgher <alirazaasgher21@gmail.com>
2026-01-04 01:19:44 +0500

    new changes

--- app/Http/Resources/CompetitorResource.php
+++ app/Http/Resources/CompetitorResource.php
@@ -8,13 +8,13 @@ class CompetitorResource extends JsonResource
     public function toArray($request)
     {
         $request->merge(['from_competitor' => true]);
-        $baseUrl = config('app.url');
+        $baseUrl = config('app.asset_url');
         return [
             'id' => $this->id,
             'slug' => $this->slug,
             'brand' => $this->brand,
             'name' => $this->name,
-            'primary_image' => $this->primary_image ? $baseUrl . '/storage/' . ltrim($this->primary_image, '/')
+            'primary_image' => $this->primary_image ? $baseUrl . '/' . ltrim($this->primary_image, '/')
                 : null,
             'searchIndex' => new PhoneSearchResource($this->whenLoaded('searchIndex'), hideDetails: true),
         ];
