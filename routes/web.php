bb5a21a8a8681d4e5a89dce39dc6b5dfa95174b4
alirazaasgher <alirazaasgher21@gmail.com>
2026-01-06 23:15:45 +0500

    new changes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
-
 <?php
 
 use App\Http\Controllers\Admin\MobileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'ok',
+        'timestamp' => now()->toDateTimeString(),
+        'service' => 'API'
+    ]);
+});
 Route::group(['prefix' => 'admin'], function () {
-     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('mobiles', MobileController::class);
 });
