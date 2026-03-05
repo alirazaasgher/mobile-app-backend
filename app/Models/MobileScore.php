0f5eb38232e0eecd60a8d735f5d704ce929aea04
alirazaasgher <alirazaasgher21@gmail.com>
2026-03-06 00:28:38 +0500

    new changes

--- app/Models/MobileScore.php
+++ app/Models/MobileScore.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class MobileScore extends Model
 {
     protected $casts = [
-        'score' => 'float',
+        'score' => 'decimal:2',
         'breakdown' => 'array',
     ];
     protected $fillable = [
